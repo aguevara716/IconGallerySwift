2ba31166ea32c6cbe91e45d37a6afd34859fe915
Angel Guevara, Jr <aguevara.716@gmail.com>
2022-01-30 12:33:51 -0500

    Initial Commit

---
+++ IconGallery/IconGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconGalleryApp.swift
+//  IconGallery
+//
+//  Created by Angel Guevara, Jr. on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct IconGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
